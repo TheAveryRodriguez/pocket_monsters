298edaf2154ad507177c18f2c38a8374751fbc0a
Edward Rodriguez <rodriguez.avery@gmail.com>
2023-10-12 21:07:55 -0500

    US 8 - child index link partial

--- spec/features/pocket_monsters/index_spec.rb
+++ spec/features/pocket_monsters/index_spec.rb
@@ -5,11 +5,12 @@ RSpec.describe "pocket monsters index page" do
     @trainer1 = Trainer.create(name: "Ash", age: 21, leader: true)
     @pocket_monster1 = PocketMonster.create(name: "Pikachu", level: 100, captured: true, trainer_id: @trainer1.id)
     @pocket_monster2 = PocketMonster.create(name: "Charizard", level: 99, captured: true, trainer_id: @trainer1.id)
+
+    visit "/pocket_monsters"
   end
+
   describe "when i visit the pocket monsters index" do
     it "I see the name of each pocket monster record in the system" do
-      visit "/pocket_monsters"
-
       expect(page).to have_content("Name: #{@pocket_monster1.name}")
       expect(page).to have_content("Level: #{@pocket_monster1.level}")
       expect(page).to have_content("Captured: #{@pocket_monster1.captured}")
@@ -19,6 +20,13 @@ RSpec.describe "pocket monsters index page" do
       expect(page).to have_content("Level: #{@pocket_monster2.level}")
       expect(page).to have_content("Captured: #{@pocket_monster2.captured}")
       expect(page).to have_content("Trainer: #{@pocket_monster2.trainer_id}")
+    end
+  end
+
+  describe "US 8 - Child Index Link" do
+    it "Every page has a link at the top of the page to the Pocket Monsters index" do
+      save_and_open_page
+      expect(page).to have_link("All Pocket Monsters")
     end
   end
 end
